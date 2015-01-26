226566eb34502b50be05bc4bf5187e19b259083a
Boom3k <boomboom3k@gmail.com>
2015-01-25 19:34:51 -0800

    Hello

--- TestHello/TestHello/Program.cs
+++ TestHello/TestHello/Program.cs
@@ -10,9 +10,10 @@ namespace TestHello
     {
         static void Main(string[] args)
         {
-            System. Console.WriteLine("Hello World!");
+            //System. Console.WriteLine("Hello World!"); //This is how you comment. You use "//". On that note, this line is wrong lol.. I am going to comment it out
             System.Console.WriteLine("Gurl got a big ol booty");
             System.Console.WriteLine("Code line add from Chris.");
+            System.Console.WriteLine("Boom Boom Boom Boom is who im is negro!!!");
         }
     }
 }
